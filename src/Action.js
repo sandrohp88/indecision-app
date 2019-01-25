6fd236634950c653f654cca86acc775be5f9c975
Sandro Herrera Pallares <sandrohp88@gmail.com>
2019-01-25 13:53:33 -0800

    Add Action component

---
+++ src/Action.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+class Action extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onMakeDecision = this.onMakeDecision.bind(this);
+  }
+
+  onMakeDecision() {
+    const randomNumber = Math.floor(Math.random() * this.props.options.length);
+    alert(this.props.options[randomNumber])
+  }
+
+  render() {
+    return (
+      <button
+        disabled={this.props.options.length === 0}
+        onClick={this.onMakeDecision}
+      >
+        What should I do
+      </button>
+    );
+  }
+}
+export {Action}
