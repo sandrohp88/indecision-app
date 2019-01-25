cf179aee123d2f90441ebc83be903a0a7a4f8e82
Sandro Herrera Pallares <sandrohp88@gmail.com>
2019-01-25 13:54:41 -0800

    Render All components in App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
+import logo from "./logo.svg";
+import "./App.css";
+
+import {Header} from './Header'
+import { Action } from "./Action";
+import { Options } from "./Options";
+import { AddOption } from "./AddOption";
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { options: [] };
+    this.removeAllHandler = this.removeAllHandler.bind(this);
+    this.addOption = this.addOption.bind(this)
+    
+  }
+  addOption(option){
+    this.state.options.push(option)
+    this.setState({options: this.state.options})
+  }
+  removeAllHandler() {
+    this.setState({ options: [] });
+  }
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Header />
+        <p>{this.state.options.length > 0 && "Here are your options"}</p>
+        <Action options={this.state.options}/>
+        <button onClick={this.removeAllHandler}>Remove all</button>
+        <Options options = {this.state.options}/>
+        <AddOption addOption={this.addOption}/>
       </div>
     );
   }
