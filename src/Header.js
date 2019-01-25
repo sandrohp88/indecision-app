682f511d5254c942d3a7d43be4ccc5f0f2b3e738
Sandro Herrera Pallares <sandrohp88@gmail.com>
2019-01-25 13:51:33 -0800

    Add header component

---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+const Header = () =>(
+  <React.Fragment>
+    <h1>Indecision App</h1>
+    <p>Put your life in the hands of a computer</p>
+  </React.Fragment>
+)
+
+export {Header}
