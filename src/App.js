7ee7248e43c4f57fe2744f4ed49c7957f33dd0f1
Sandro Herrera Pallares <sandrohp88@gmail.com>
2019-01-25 16:20:13 -0800

    Add some life-cycle method and use localstorage for data persintance

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 
-import {Header} from './Header'
+import { Header } from "./Header";
 import { Action } from "./Action";
 import { Options } from "./Options";
 import { AddOption } from "./AddOption";
@@ -12,26 +12,61 @@ class App extends Component {
     super(props);
     this.state = { options: [] };
     this.removeAllHandler = this.removeAllHandler.bind(this);
-    this.addOption = this.addOption.bind(this)
-    
+    this.addOption = this.addOption.bind(this);
+    this.removeOption = this.removeOption.bind(this);
   }
-  addOption(option){
-    this.state.options.push(option)
-    this.setState({options: this.state.options})
+  componentDidMount() {
+    const json = localStorage.getItem("options");
+    const options = JSON.parse(json);
+    if (options) {
+      //set initial state
+      // const initialState = JSON.parse(options)
+      this.setState({ options });
+    } else {
+      const initialOptions = JSON.stringify({ options: [] });
+      localStorage.setItem("options", initialOptions);
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.options.length !== this.state.options.length) {
+      const json = JSON.stringify(this.state.options);
+      localStorage.setItem("options", json);
+    }
+  }
+  addOption(option) {
+    if (!option) {
+      console.log("enter valid option");
+      return "Enter a valid option";
+    } else if (this.state.options.includes(option)) {
+      return `${option} already exits`;
+    }
+    this.setState(prevState => {
+      return { options: prevState.options.concat(option) };
+    });
   }
   removeAllHandler() {
     this.setState({ options: [] });
   }
-  
+  removeOption(option) {
+    const found = this.state.options.includes(option);
+    if (found) {
+      this.setState(prevState => {
+        return { options: prevState.options.filter(opt => opt !== option) };
+      });
+    }
+  }
+
   render() {
     return (
       <div>
-        <Header />
-        <p>{this.state.options.length > 0 && "Here are your options"}</p>
-        <Action options={this.state.options}/>
-        <button onClick={this.removeAllHandler}>Remove all</button>
-        <Options options = {this.state.options}/>
-        <AddOption addOption={this.addOption}/>
+        <Header numberOfOptions={this.state.options.length} />
+        <Action options={this.state.options} />
+        <Options
+          options={this.state.options}
+          removeAllOptions={this.removeAllHandler}
+          removeOption={this.removeOption}
+        />
+        <AddOption addOption={this.addOption} />
       </div>
     );
   }
