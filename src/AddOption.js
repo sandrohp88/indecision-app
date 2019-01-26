0bfecb28ee6f576f0350c97f7da22fa9ce76ea8f
Sandro Herrera Pallares <sandrohp88@gmail.com>
2019-01-25 16:16:02 -0800

    Add state to AddOption component

--- src/AddOption.js
+++ src/AddOption.js
@@ -1,30 +1,34 @@
-import React from 'react'
+import React from "react";
 
-class AddOption extends React.Component{
-  constructor(props){
-    super(props)
+class AddOption extends React.Component {
+  constructor(props) {
+    super(props);
     this.onFormSubmit = this.onFormSubmit.bind(this);
-
+    this.state = { error: "" };
   }
   onFormSubmit(event) {
     event.preventDefault();
     const option = event.target.elements.option.value;
-    if (option) {
-      this.props.addOption(option)
+
+    event.target.elements.option.value = "";
+    const error = this.props.addOption(option);
+    this.setState({ error });
+    if (!error) {
       event.target.elements.option.value = "";
     }
   }
 
   render() {
     return (
-      <React.Fragment>
+      <div>
+        {this.state.error && <p>{this.state.error}</p>}
         <form onSubmit={this.onFormSubmit}>
           <input type="text" name="option" id="option" />
           <button type="submit">Add option</button>
         </form>
-      </React.Fragment>
-    )
+      </div>
+    );
   }
 }
 
-export {AddOption}
+export { AddOption };
