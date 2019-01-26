6b1aaed8c005d99305c91a6bb7b527fa62f3fdfe
Sandro Herrera Pallares <sandrohp88@gmail.com>
2019-01-25 16:18:01 -0800

    Change from class component to stateless component

--- src/Options.js
+++ src/Options.js
@@ -1,21 +1,13 @@
 import React from "react";
 import { Option } from "./Option";
-
-class Options extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <React.Fragment>
-        <ol>
-          {this.props.options.map((option, i) => (
-            <Option key={option + "__" + i} opt={option} />
-          ))}
-        </ol>
-      </React.Fragment>
-    );
-  }
-}
-
-export {Options}
+const Options = props => (
+  <div>
+  <button onClick={props.removeAllOptions}>Remove all</button>
+    <ol>
+      {props.options.map((option, i) => (
+        <Option key={option + "__" + i} opt={option} remove={props.removeOption} />
+      ))}
+    </ol>
+  </div>
+);
+export { Options };
