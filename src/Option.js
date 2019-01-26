5e4d08e669c648747890d50e853c62b933cf7f8e
Sandro Herrera Pallares <sandrohp88@gmail.com>
2019-01-25 16:17:32 -0800

    Change from class component to a stateless component

--- src/Option.js
+++ src/Option.js
@@ -1,3 +1,9 @@
-import React from 'react'
-const Option = props => <li>{props.opt}</li>
-export {Option}
+import React from "react";
+const Option = props => (
+  <div>
+    <li>{props.opt}</li>
+    <button onClick={() => props.remove(props.opt)}>X</button>
+  </div>
+);
+
+export { Option };
